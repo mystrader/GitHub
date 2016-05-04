0f23fe78d9d2ebf4efe3a26e3f7f851236b73582
Marcus Cunha <mystrader@gmail.com>
2016-05-04 00:54:25 -0300

    estudo funções

--- projetos/testGrunt/js/a.js
+++ projetos/testGrunt/js/a.js
@@ -1,1 +1,9 @@
-teste1
+var produto = {nome: 'Sapato', preço: '150'}
+
+var formulaImpostoA  = function (preco){
+    return preco*0.1;
+}
+var formulaImpostoA  = function (preco){
+    return preco*0.2;
+}
+
